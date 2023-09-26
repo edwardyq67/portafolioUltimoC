3b5b4fd4eb82acc59057a06204d1af7645dcabf1
edward1 <edwardyq67@gmail.com>
2023-09-25 23:33:29 -0500

    sedas

--- src/paginacion/Conoceme.jsx
+++ src/paginacion/Conoceme.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./css/conoceme.css";
 import fondoC from "../img/react.jpg";
+import yo from "../img/WhatsApp Image 2023-09-25 at 11.30.42 PM.jpeg"
 const Conoceme = () => {
   return (
     <div  className="conoceme">
       <div  className="capa">
         <div className="cajaConoceme">
           <div className="img">
-            <img data-aos="zoom-in" src={fondoC} alt="" />
+            <img data-aos="zoom-in" src={yo} alt="" />
           </div>
-          <div data-aos="fade-left" className="inf">
+          <div  className="inf">
             <h1>Hola!</h1>
             <h4>Soy Edward Yllanes Quirari</h4>
             <div className="todo-conoceme">
